78da6ae49ff5b40b48ee148ce422f7ef00b50288
Siruzc10 <joseph.uzcategui2008@gmail.com>
2026-06-25 08:25:41 -0300

    Clase.N°8-Joseph_Uzcátegui.py

--- Creación-Clases/Clase.N°8-Joseph_Uzcátegui.py
+++ Creación-Clases/Clase.N°8-Joseph_Uzcátegui.py
@@ -1,5 +1,5 @@
 
-# Crea una clase "Tienda" con propiedades para el nombre de la tienda y una lista de productos disponibles, 
+# 8. Crea una clase "Tienda" con propiedades para el nombre de la tienda y una lista de productos disponibles, 
 # y métodos para añadir o eliminar productos de la lista y para obtener la lista completa de productos.
 
 class Tienda:
