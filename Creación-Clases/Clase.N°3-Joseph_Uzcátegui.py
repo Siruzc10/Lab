1da92cd7da75871a5ab3f5295d857cc2ba07cd2c
Siruzc10 <joseph.uzcategui2008@gmail.com>
2026-06-25 08:23:31 -0300

    Clase.N°3-Joseph_Uzcátegui.py

--- Creación-Clases/Clase.N°3-Joseph_Uzcátegui.py
+++ Creación-Clases/Clase.N°3-Joseph_Uzcátegui.py
@@ -2,57 +2,29 @@
 # 3. Crea una clase "Producto" con propiedades para el nombre, el precio y el stock disponible, 
 # y métodos para aumentar o disminuir el stock.
 
-class Rectangulo:
-    def __init__(self, longitud, anchura):
-        # Inicializo las propiedades del rectángulo
-        self.longitud = longitud
-        self.anchura = anchura
+class Producto:
 
-    def obtener_longitud(self):
-        return self.longitud
+    def __init__(self, nombre, precio, stock):
+        self._nombre = nombre
+        self._precio = precio
+        self._stock = stock
 
-    def establecer_longitud(self, longitud_nueva):
-        self.longitud = longitud_nueva
+    def aumentar_stock(self, cantidad):
+        self._stock += cantidad
 
-    def obtener_anchura(self):
-        return self.anchura
+    def disminuir_stock(self, cantidad):
+        if cantidad <= self._stock:
+            self._stock -= cantidad
 
-    def establecer_anchura(self, anchura_nueva):
-        self.anchura = anchura_nueva
-
-    def calcular_area(self):
-        # area = longitud * anchura
-        return self.longitud * self.anchura
-
-    def calcular_perimetro(self):
-        # Perimetro = 2 * (longitud + anchura)
-        return 2 * (self.longitud + self.anchura)
+    def obtener_stock(self):
+        return self._stock
 
 
 if __name__ == "__main__":
-    # Creo instancia de Rectangulo
-    rectangulo1 = Rectangulo(10, 5)
+    producto = Producto("Mouse", 5000, 10)
 
-    # Mostrar propiedades
-    print(f"Longitud: {rectangulo1.obtener_longitud()}")
-    print(f"Anchura: {rectangulo1.obtener_anchura()}")
+    producto.aumentar_stock(5)
+    producto.disminuir_stock(3)
 
-    # Mostrar los calculos
-    print(f"Area: {rectangulo1.calcular_area()}")
-    print(f"Perimetro: {rectangulo1.calcular_perimetro()}")
-
-    # Modificar valores
-    print("Establece nuevos valores")
-    nueva_longitud = int(input("Longitud: "))
-    nueva_anchura = int(input("Anchura: "))
-
-    rectangulo1.establecer_longitud(nueva_longitud)
-    rectangulo1.establecer_anchura(nueva_anchura)
-
-    # Mostrar valores actualizados
-    print("\nDespues de actualizar:")
-    print(f"Longitud: {rectangulo1.obtener_longitud()}")
-    print(f"Anchura: {rectangulo1.obtener_anchura()}")
-    print(f"Area: {rectangulo1.calcular_area()}")
-    print(f"Perímetro: {rectangulo1.calcular_perimetro()}")
+    print("Stock:", producto.obtener_stock())
  
