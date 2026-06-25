5a27cd10227e2e66c6dc868510dd4dc6fb75aac0
Siruzc10 <joseph.uzcategui2008@gmail.com>
2026-06-25 08:27:44 -0300

    Clase.N°5-Joseph_Uzcátegui.py

--- Creación-Clases/Clase.N°5-Joseph_Uzcátegui.py
+++ Creación-Clases/Clase.N°5-Joseph_Uzcátegui.py
@@ -1,72 +1,45 @@
+# 5. Crea una clase "Círculo" con propiedades para el radio y el diámetro, 
+# y métodos para calcular el área y la circunferencia del círculo.
 
-# 6. Crea una clase "Estudiante" con propiedades para el nombre, la edad y la carrera, 
-# y métodos para obtener y establecer estas propiedades, 
-# así como un método para calcular la nota media de un conjunto de exámenes.
+class Circulo:
+    def __init__(self, radio):
+        # Inicializo la propiedad del circulo
+        self.radio = radio
 
-class Estudiante:
-    def __init__(self, nombre, edad, carrera):
-        # Inicializo propiedades
-        self.nombre = nombre
-        self.edad = edad
-        self.carrera = carrera
+    def obtener_radio(self):
+        return self.radio
 
-    def obtener_nombre(self):
-        return self.nombre
+    def establecer_radio(self, radio_nuevo):
+        self.radio = radio_nuevo
 
-    def establecer_nombre(self, nombre_nuevo):
-        self.nombre = nombre_nuevo
+    def obtener_diametro(self):
+        return self.radio * 2
 
-    def obtener_edad(self):
-        return self.edad
+    def calcular_area(self):
+        # pi * radio al cuadrado
+        return 3.1416 * (self.radio ** 2)
 
-    def establecer_edad(self, edad_nueva):
-        self.edad = edad_nueva
+    def calcular_circunferencia(self):
+        # 2 * pi * radio
+        return 2 * 3.1416 * self.radio
 
-    def obtener_carrera(self):
-        return self.carrera
-    
-    def establecer_carrera(self, carrera_nueva):
-        self.carrera = carrera_nueva
-
-    
-    def calcular_nota_media(self, notas):
-        if len(notas) == 0:
-            return 0
-        return sum(notas) / len(notas)
 
 if __name__ == "__main__":
     # Creo instancia
-    estudiante1 = Estudiante("Joseph", 17, "Programador")
+    circulo1 = Circulo(5)
 
-    # Mostrar propiedades
-    print(f"Nombre: {estudiante1.obtener_nombre()}")
-    print(f"Edad: {estudiante1.obtener_edad()}")
-    print(f"Carrera: {estudiante1.obtener_carrera()}")
-    print("Nota media: 9")
+    print(f"Radio: {circulo1.obtener_radio()}")
+    print(f"Diámetro: {circulo1.obtener_diametro()}")
 
-    # Modificar valores
-    print("Establece un Nuevo Alumno:")
-    nuevo_nombre = input("Nombre: ")
-    nueva_edad = int(input("Edad: "))
-    nueva_carrera =  input("Carrera: ")
+    print(f"Área: {circulo1.calcular_area()}")
+    print(f"Circunferencia: {circulo1.calcular_circunferencia()}")
 
-    estudiante1.establecer_nombre(nuevo_nombre)
-    estudiante1.establecer_edad(nueva_edad)
-    estudiante1.establecer_carrera(nueva_carrera)
+    print("Establece nuevo radio")
+    nuevo_radio = int(input("Radio: "))
+    circulo1.establecer_radio(nuevo_radio)
 
-    notas = []
-    print("\nIngrese las notas (ingrese -1 para terminar):")
-
-    while True:
-        nota = float(input("Nota: "))
-        if nota == -1:
-            break
-        notas.append(nota)
-
-
-    # Mostrar valores actualizados
     print("\nDespués de actualizar:")
-    print(f"Nombre:{estudiante1.obtener_nombre()}")
-    print(f"Edad: {estudiante1.obtener_edad()}")
-    print(f"Carrera: {estudiante1.obtener_carrera()}")
-    print(f"Nota media: {estudiante1.calcular_nota_media(notas)}")
+    print(f"Radio: {circulo1.obtener_radio()}")
+    print(f"Diámetro: {circulo1.obtener_diametro()}")
+    print(f"Área: {circulo1.calcular_area()}")
+    print(f"Circunferencia: {circulo1.calcular_circunferencia()}")
