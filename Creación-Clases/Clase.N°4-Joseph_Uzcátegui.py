4fc1812f3034a8e28ff472b92aba3f3cd33c117b
Siruzc10 <joseph.uzcategui2008@gmail.com>
2026-06-25 08:28:57 -0300

    Clase.N°4-Joseph_Uzcátegui.py

--- Creación-Clases/Clase.N°4-Joseph_Uzcátegui.py
+++ Creación-Clases/Clase.N°4-Joseph_Uzcátegui.py
@@ -1,46 +1,21 @@
+# 4. Crea una clase "Rectángulo" con propiedades para la longitud y la anchura, 
+# y métodos para calcular el área y el perímetro del rectángulo.
 
-# 5. Crea una clase "Círculo" con propiedades para el radio y el diámetro, 
-# y métodos para calcular el área y la circunferencia del círculo.
+class Rectangulo:
 
-class Circulo:
-    def __init__(self, radio):
-        # Inicializo la propiedad del circulo
-        self.radio = radio
+    def __init__(self, longitud, anchura):
+        self._longitud = longitud
+        self._anchura = anchura
 
-    def obtener_radio(self):
-        return self.radio
+    def area(self):
+        return self._longitud * self._anchura
 
-    def establecer_radio(self, radio_nuevo):
-        self.radio = radio_nuevo
-
-    def obtener_diametro(self):
-        return self.radio * 2
-
-    def calcular_area(self):
-        # pi * radio al cuadrado
-        return 3.1416 * (self.radio ** 2)
-
-    def calcular_circunferencia(self):
-        # 2 * pi * radio
-        return 2 * 3.1416 * self.radio
+    def perimetro(self):
+        return 2 * (self._longitud + self._anchura)
 
 
 if __name__ == "__main__":
-    # Creo instancia
-    circulo1 = Circulo(5)
+    rect = Rectangulo(10, 5)
 
-    print(f"Radio: {circulo1.obtener_radio()}")
-    print(f"Diámetro: {circulo1.obtener_diametro()}")
-
-    print(f"Área: {circulo1.calcular_area()}")
-    print(f"Circunferencia: {circulo1.calcular_circunferencia()}")
-
-    print("Establece nuevo radio")
-    nuevo_radio = int(input("Radio: "))
-    circulo1.establecer_radio(nuevo_radio)
-
-    print("\nDespués de actualizar:")
-    print(f"Radio: {circulo1.obtener_radio()}")
-    print(f"Diámetro: {circulo1.obtener_diametro()}")
-    print(f"Área: {circulo1.calcular_area()}")
-    print(f"Circunferencia: {circulo1.calcular_circunferencia()}")
+    print("Área:", rect.area())
+    print("Perímetro:", rect.perimetro())
